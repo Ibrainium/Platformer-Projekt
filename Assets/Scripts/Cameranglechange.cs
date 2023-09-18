a40071e222f07bd81a5f0c499950384a04a455a2
byggemandb <basmalayj@hotmail.com>
2023-09-18 22:43:52 +0200

    Cavecamera

---
+++ Assets/Scripts/Cameranglechange.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameranglechange : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Camera CaveCamera;
+
+    void Start()
+    {
+        MainCamera.enabled = true;
+        CaveCamera.enabled = false;
+    }
+
+    public void ShowCaveCamera()
+    {
+        MainCamera.enabled = false;
+        CaveCamera.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("pirate_man"))
+        {
+            ShowCaveCamera();
+
+        }
+        else
+        {
+            Debug.LogWarning("Object with tag 'pirate_man' not found.");
+        }
+    }
+}
